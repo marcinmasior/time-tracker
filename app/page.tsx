2f2123556f8f260a34a9c30350697f4930b51a22
Marcin Mąsior <marcinmasior@gmail.com>
2023-10-26 16:18:23 +0200

    Add Chakra UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,27 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import {Button, FormControl, FormLabel, ListItem, Spinner, Switch, UnorderedList} from '@chakra-ui/react'
+import { Code } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
+      <h1>Hello World from next js app</h1>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div>
+        <h2>Here are some example components:</h2>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <UnorderedList>
+          <ListItem><Button colorScheme='blue'>Button</Button></ListItem>
+          <ListItem><Code colorScheme='red' children="var chakra = 'awesome!'" /></ListItem>
+          <ListItem>
+            <FormControl display='flex' alignItems='center'>
+              <FormLabel htmlFor='email-alerts' mb='0'>
+                Enable email alerts?
+              </FormLabel>
+              <Switch id='email-alerts' />
+            </FormControl>
+          </ListItem>
+          <ListItem><Spinner /></ListItem>
+        </UnorderedList>
       </div>
     </main>
   )
